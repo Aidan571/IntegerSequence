fe583ead611bb5e9996167873fd684972563fa60
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-12-08 12:58:26 -0500

    Created 4 methods from IntegerSequence interface.

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,14 +1,39 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   private int currentIndex;
-  private int []data;
+  private int[] data;
 
   public ArraySequence(int[] other){
     currentIndex = 0;
+    data = new int[other.length];
     for(int i = 0; i < other.length; i++){
       data[i] = other[i];
     }
   }
 
-  
+  public void reset(){
+    this.currentIndex = 0;
+  }
+
+  public int length(){
+    return this.data.length;
+  }
+
+  public boolean hasNext(){
+    if(currentIndex < this.length()){
+      return true;
+    }
+    else
+    return false;
+  }
+
+  public int next() throws NoSuchElementException{
+    if(this.hasNext()){
+      int holder = this.data[currentIndex];
+      currentIndex += 1;
+      return holder;
+    }
+    else
+    throw new NoSuchElementException("There is no next value.");
+  }
 }
